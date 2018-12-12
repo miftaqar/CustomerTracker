54ab20cc2deccdb97cd7e761d4d2fdb74aabffcb
mifteqar <ifteqar.ahmed01@gmail.com>
2018-12-12 12:44:57 +0530

    Added Hibernate Configurations in java congif files, Created sample jsp page and Customer controller class for testing

--- src/com/primetgi/org/config/CustomerConfigApp.java
+++ src/com/primetgi/org/config/CustomerConfigApp.java
@@ -1,15 +1,25 @@
 package com.primetgi.org.config;
 
+import java.beans.PropertyVetoException;
+import java.util.Properties;
+
+import org.hibernate.SessionFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "com.primetgi.org.controller")
+@EnableTransactionManagement
 public class CustomerConfigApp {
 
 	@Bean
@@ -20,6 +30,52 @@ public class CustomerConfigApp {
 		viewResolver.setSuffix(".jsp");
 
 		return viewResolver;
+	}
+
+	/*
+	 * Configuring Hibernate Connection Pool
+	 */
+
+	// Step 1: Define Database Datasource / connection pool
+
+	@Bean
+	public ComboPooledDataSource comboPooledDataSource() throws PropertyVetoException {
+		ComboPooledDataSource dataSource = new ComboPooledDataSource();
+		dataSource.setDriverClass("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+		dataSource.setJdbcUrl("jdbc:sqlserver://user-229;databaseName=user229DB");
+		dataSource.setUser("sa");
+		dataSource.setPassword("admin");
+		dataSource.setMinPoolSize(5);
+		dataSource.setMaxPoolSize(20);
+		dataSource.setMaxIdleTime(30000);
+		return dataSource;
+	}
+
+	// Step 2: Set up Hibernate Session Factory
+	@Bean
+	public LocalSessionFactoryBean localSessionFactoryBean() throws PropertyVetoException {
+		LocalSessionFactoryBean sessionFactoryBean = new LocalSessionFactoryBean();
+		sessionFactoryBean.setDataSource(comboPooledDataSource());
+		sessionFactoryBean.setPackagesToScan(new String[] { "com.primetgi.org.entity" });
+		sessionFactoryBean.setHibernateProperties(hibernateProperties());
+		return sessionFactoryBean;
+
+	}
+
+	private Properties hibernateProperties() {
+		Properties properties = new Properties();
+		properties.setProperty("hibernate.hbm2ddl.auto", "update");
+		properties.setProperty("hibernate.dialect", "org.hibernate.dialect.SQLServerDialect");
+		properties.setProperty("hibernate.show_sql", "true");
+		return properties;
+	}
+
+	// Step 3: Set Up Hibernate Transactional Manager
+
+	public HibernateTransactionManager hibernateTransactionManager() throws PropertyVetoException {
+		HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+		transactionManager.setSessionFactory((SessionFactory) localSessionFactoryBean());
+		return transactionManager;
 	}
 
 }
